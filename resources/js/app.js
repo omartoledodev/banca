49d2303323b79b3ac890152fe03b32b20150ec77
Omar Toledo <omar1566@gmail.com>
2023-04-28 18:40:59 -0600

    Comenzando proyecto

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+import { createApp } from 'vue'
+//import Welcome from './components/Welcome'
+import Welcome from './components/Welcome.vue'
+
+const app = createApp({})
+
+app.component('welcome', Welcome)
+
+app.mount('#app')
